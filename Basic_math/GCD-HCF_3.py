98b1129d137d878fc2b615f501e7737539f4eb7b
shubhamnakum <nakumshubham@gmail.com>
2026-07-11 00:10:08 +0530

    bug fix in gcd 3

--- Basic_math/GCD-HCF_3.py
+++ Basic_math/GCD-HCF_3.py
@@ -21,17 +21,18 @@ else
 def find_gcd(num1,num2):
     a=num1
     b=num2
-
-    while (a>0 & b>0):
+    while (a>0) & (b>0):
         if (a>b):
             a=a%b
         else:
             b=b%a
+    print(f"a = {a}, b={b}")
     if a==0:
         gcd = b
-    else:
+        return gcd
+    elif b==0:
         gcd = a
-    return gcd
+        return gcd
 
 n1 = int(input("Enter 1st Number: "))
 n2 = int(input("Enter 2nd Number: "))
@@ -39,4 +40,4 @@ n2 = int(input("Enter 2nd Number: "))
 result = find_gcd(n1,n2)
 print(f"GCD of  {n1} & {n2} is, {result}")
 
-# Time Complexity : O(min(a,b))
+# Time Complexity : O(log(min(a,b))) [base phi]
