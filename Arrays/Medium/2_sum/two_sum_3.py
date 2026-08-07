8ed439bb71ab869c566b92c86946adfb2a428f45
shubhamnakum <nakumshubham@gmail.com>
2026-08-08 00:09:48 +0530

    update : 2 sum problem

--- Arrays/Medium/2_sum/two_sum_3.py
+++ Arrays/Medium/2_sum/two_sum_3.py
@@ -1,38 +1,45 @@
-# This function is to find the two elements that sums upto the given target, using Hashing approach:
+# This function is to find the two elements that sums upto the given target, using 2 pointers approach:
+# Note: This approach can only check if there exists any 2 number that sums to target, it cant return the indices of it.
 
 # Pseudo Code:
 """
 a=[2,6,8,5,11]
 
-
-for i = 0 --> n:
-    rem = target - sum
-    if rem in hash:
-        return [hash[map]],i]
-    hash[arr] = i
-    retutn []
+a.sort()
+l,r = 0,n-1
+while(l>r):
+    sum = arr(l) + arr(r)
+    if  sum == target:
+        print(yes)
+        break
+    else if : sum > target:
+        r--
+    else if : sum < target:
+        l++
 """
 
 
 # implementation:
 
 def two_sum(a,target):
-    n=len(a)
-    hash = {}
-    for i in range(n):
-        sum = a[i]
-        rem = target - sum
-        if rem in hash:
-            return (hash[rem],i)
-        hash[a[i]] = i
-    return -1,-1
+    a.sort()
+    l,r = 0, (len(a)-1)
+    while(l<r):
+        sum = a[l] + a[r]
+        if sum > target:
+            r-=1
+        elif sum < target:
+            l+=1
+        else:
+            print(f"yes, the sum of {a[l]} and {a[r]} is {sum} that matches the target : {target}")
+            break
+    return None
 
 
 a = [2,6,8,5,11]
 target = 14
-id_1,id_2 = two_sum(a,target)
-print(f"the indices are: {id_1} & {id_2}")
+two_sum(a,target)
 
 
-# T.C = o(n)
-# S.C = o(n)
+# T.C = o(n) + o(nlogn)
+# S.C = o(1)
